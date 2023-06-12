75bb59c251855d2d5486807aed5d4cc5cfa8102f
Elizabeth Kenyon <elizabeth.kenyon@shopify.com>
2023-06-12 15:19:22 -0500

    Update Function Code

--- extensions/js-hide-payment-method/src/index.js
+++ extensions/js-hide-payment-method/src/index.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+// Use JSDoc annotations for type safety
 /**
 * @typedef {import("../generated/api").InputQuery} InputQuery
 * @typedef {import("../generated/api").FunctionResult} FunctionResult
@@ -13,45 +14,38 @@ const NO_CHANGES = {
   operations: [],
 };
 
+// The @shopify/shopify_function package will use the default export as your function entrypoint
 export default /**
-* @param {InputQuery} input
-* @returns {FunctionResult}
-*/
-(input) => {
-  // Define a type for your configuration, and parse it from the metafield
-  /**
-  * @type {{
-  *   paymentMethodName: string
-  *   cartTotal: number
-  * }}
-  */
-  const configuration = JSON.parse(
-    input?.paymentCustomization?.metafield?.value ?? "{}"
-  );
-  if (!configuration.paymentMethodName || !configuration.cartTotal) {
-    return NO_CHANGES;
-  }
+    * @param {InputQuery} input
+    * @returns {FunctionResult}
+    */
+// [START payment_customization-hide-payment-method.using_input_value]
+  (input) => {
+    // Get the cart total from the function input, and return early if it's below 100
+    const cartTotal = parseFloat(input.cart.cost.totalAmount.amount ?? "0.0");
+    if (cartTotal < 100) {
+      // You can use STDERR for debug logs in your function
+      console.error("Cart total is not high enough, no need to hide the payment method.");
+      return NO_CHANGES;
+    }
 
-  const cartTotal = parseFloat(input.cart.cost.totalAmount.amount ?? "0.0");
-  // Use the configured cart total instead of a hardcoded value
-  if (cartTotal < configuration.cartTotal) {
-    console.error("Cart total is not high enough, no need to hide the payment method.");
-    return NO_CHANGES;
-  }
+    // Find the payment method to hide
+    const hidePaymentMethod = input.paymentMethods
+      .find(method => method.name.includes("Cash on Delivery"));
+    if (!hidePaymentMethod) {
+      return NO_CHANGES;
+    }
+// [END payment_customization-hide-payment-method.using_input_value]
 
-  // Use the configured payment method name instead of a hardcoded value
-  const hidePaymentMethod = input.paymentMethods
-    .find(method => method.name.includes(configuration.paymentMethodName));
-
-  if (!hidePaymentMethod) {
-    return NO_CHANGES;
-  }
-
-  return {
-    operations: [{
-      hide: {
-        paymentMethodId: hidePaymentMethod.id
-      }
-    }]
+    // The @shopify/shopify_function package applies JSON.stringify() to your function result
+    // and writes it to STDOUT
+// [START payment_customization-hide-payment-method.return_operations]
+    return {
+      operations: [{
+        hide: {
+          paymentMethodId: hidePaymentMethod.id
+        }
+      }]
+    };
   };
-};
+// [END payment_customization-hide-payment-method.return_operations]
